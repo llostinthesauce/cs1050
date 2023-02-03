321ceaa5b2c81b7c44b719fc0e03bcd4cbdae841
llostinthesauce <90298057+llostinthesauce@users.noreply.github.com>
2023-02-03 17:16:57 -0600

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -1,6 +1,6 @@
-  1 /* CS1050 1050
+  1 /* CS1050
   2 * SP 2023
-  3 * * Lab 3
+  3 * * Lab 3 
   4 * * By Corbin Shanks (cwswgv)
   5 * * Febuary 3, 2023
   6 * *****************************************************************************/
@@ -10,27 +10,28 @@
  10 int main(void) /* initalizes main to begin program */
  11 {
  12     int count; /* declares count as int type vartiable */
- 13     count = 101; /* sets count to 101 */
+ 13     count = 101; /* sets value in count to 101 */
  14 
  15     while (count > 1) /* while count is greater than 1, loop the following code */
  16     {
- 17         count = count - 2; /* decrements count by 2 so that even numbers are filters */
- 18 
- 19         if (count % 3 == 0 && count % 7 == 0) /* says that if count is div by 3 and 7 then to print TS and current */
- 20         {
- 21             printf("TS%d ", count); /* prints if true */
- 22         }
- 23         else  if (count % 3 == 0) /* if the above statement was false, then this will check for just / 3 */
- 24         {
- 25             printf("T%d " , count); /* prints if true */
- 26         }
- 27         else  if (count % 7 == 0) /* if neither of the above are true then checks if count is / by 7 */
- 28         {
- 29             printf("S%d ", count ); /* prints if true */
- 30         }
- 31         else
- 32             printf("%d ", count); /* prints count if none of the above are satisfied */
- 33 
- 34         }
- 35     return 0; /* retuns main to 0 to end program */
- 36 }
+ 17         count--; /* decrements count by 1, after the while loops, so that even numbers can be ignored, acting as a filter */
+ 18         count--; /* since count was set to 101, program required a 2nd decrement to make count -2 each loop */
+ 19 
+ 20         if (count % 3 == 0 && count % 7 == 0) /* if count % by 3 and 7 is true, then to print TS and current value of count */
+ 21         {
+ 22             printf("TS%d ", count); /* prints TS and current value of count if true */
+ 23         }
+ 24         else  if (count % 3 == 0) /* if the above statement was false, then this will check if count % 3 */
+ 25         {
+ 26             printf("T%d " , count); /* prints T and current value of count if the above is true */
+ 27         }
+ 28         else  if (count % 7 == 0) /* if neither of the above are true then checks if count %  7 without remainder */
+ 29         {
+ 30             printf("S%d ", count ); /* prints if the above is true */
+ 31         }
+ 32         else /* if none of the other conditions from above are true, then just print current value of count */
+ 33             printf("%d ", count); /* prints count if none of the above are satisfied */
+ 34    }
+ 35 
+ 36     return 0; /* retuns main to 0 to end program */
+ 37 }
