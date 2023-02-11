76b21c5481aac24d4678eaf2914f4de14acabe9b
wayne <90298057+llostinthesauce@users.noreply.github.com>
2023-02-11 02:17:48 -0600

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -1,1 +1,72 @@
-
+errors, not complete
+ 90             case 53:
+ 91             case 54:
+ 92             case 55:
+ 93             case 56:
+ 94             case 57:
+ 95             case 58:
+ 96             case 59:
+ 97                 printf("fifty ");
+ 98                 break;
+ 99 
+100             case 60:
+101             case 61:
+102             case 62:
+103             case 63:
+104             case 64:
+105             case 65:
+106             case 66:
+107             case 67:
+108             case 68:
+109             case 69:
+110                 printf("sixty ");
+111                 break;
+112 
+113             case 70:
+114             case 71:
+115             case 72:
+116             case 73:
+117             case 74:
+118             case 75:
+119             case 76:
+120             case 77:
+121             case 78:
+122             case 79:
+123                 printf("seventy ");
+124                 break;
+125 
+126             case 80:
+127             case 81:
+128             case 82:
+129             case 83:
+130             case 84:
+131             case 85:
+132             case 86:
+133             case 87:
+134             case 88:
+135             case 89:
+136                 printf("eighty ");
+137                 break;
+138 
+139             case 90:
+140             case 91:
+141             case 92:
+142             case 93:
+143             case 94:
+144             case 95:
+145             case 96:
+146             case 97:
+147             case 98:
+148             case 99:
+149                 printf("ninety ");
+150                 break;
+151 
+152 }
+153 
+154     }
+155 
+156 while (base < 100);
+157 
+158 return(0);
+159 
+160 }
