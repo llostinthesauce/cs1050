b52e6795c4afd8a06cbcd91b7f104e2f71046b81
wayne <90298057+llostinthesauce@users.noreply.github.com>
2023-04-22 17:20:58 -0500

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -1,4 +1,92 @@
-errors, not complete
+  1 /* CS1050
+  2  * SP 2023
+  3  * LAB 4
+  4  * Corbin Shanks, cwswgv
+  5  * Feb 10, 2023
+  6  * 
+  7  * ***********************/
+  8 
+  9 #include<stdio.h>
+ 10 
+ 11 int main(void)
+ 12 {
+ 13     int base = 11;
+ 14 
+ 15     do
+ 16     {
+ 17         base++;
+ 18         switch (base)
+ 19         {
+ 20             case 11:
+ 21                 printf("eleven, ");
+ 22                 break;
+ 23             case 12:
+ 24                 printf("twelve, ");
+ 25                 break;
+ 26             case 13:
+ 27                 printf("thirteen, ");
+ 28                 break;
+ 29             case 14:
+ 30                 printf("fourteen, ");
+ 31                 break;
+ 32             case 15:
+ 33                 printf("fifteen, ");
+ 34                 break;
+ 35             case 16:
+ 36                 printf("sixteen, ");
+ 37                 break;
+ 38             case 17:
+ 39                 printf("seventeen, ");
+ 40                 break;
+ 41             case 18:
+ 42                 printf("eighteen, ");
+ 43                 break;
+ 44             case 19:
+ 45                 printf("nineteen, ");
+ 46                 break;
+ 47 
+ 48             case 20:
+ 49             case 21:
+ 50             case 22:
+ 51             case 23:
+ 52             case 24:
+ 53             case 25:
+ 54             case 26:
+ 55             case 27:
+ 56             case 28:
+ 57             case 29:
+ 58                 printf("twenty ");
+ 59                 break;
+ 60 
+ 61             case 30:
+ 62             case 31:
+ 63             case 32:
+ 64             case 33:
+ 65             case 34:
+ 66             case 35:
+ 67             case 36:
+ 68             case 37:
+ 69             case 38:
+ 70             case 39:
+ 71                 printf("thirty ");
+ 72                 break;
+ 73 
+ 74             case 40:
+ 75             case 41:
+ 76             case 42:
+ 77             case 43:
+ 78             case 44:
+ 79             case 45:
+ 80             case 46:
+ 81             case 47:
+ 82             case 48:
+ 83             case 49:
+ 84                 printf("fourty ");
+ 85                 break;
+ 86 
+ 87             case 50:
+ 88             case 51:
+ 89             case 52:
  90             case 53:
  91             case 54:
  92             case 55:
